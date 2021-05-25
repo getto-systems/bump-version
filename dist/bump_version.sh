7434ad4966e68d975f941e55fb3b12018e179469
shun <shun.fix9@gmail.com>
2021-05-25 11:34:29 +0900

    fix: bump version

--- dist/bump_version.sh
+++ dist/bump_version.sh
@@ -10,67 +10,79 @@ if [ -z "$BUMP_IGNORE_FILE" ]; then
   BUMP_IGNORE_FILE=.bump-ignore
 fi
 
-bump_version(){
-  if [ -f .bump-version.sh ]; then
-    . .bump-version.sh
+if [ -z "$BUMP_MAJOR_FILE" ]; then
+  BUMP_MAJOR_FILE=.bump-major-release
+fi
+
+if [ -z "$BUMP_SCRIPT" ]; then
+  BUMP_SCRIPT=.bump-version.sh
+fi
+
+if [ -f $BUMP_VERSION_FILE ]; then
+  BUMP_LAST_VERSION=$(cat $BUMP_VERSION_FILE)
+else
+  BUMP_LAST_VERSION=0.0.0
+fi
+
+bump_main() {
+  bump_version
+  git commit -m "version bumped: $(cat $BUMP_VERSION_FILE)"
+}
+
+bump_version() {
+  if [ -f $BUMP_SCRIPT ]; then
+    . $BUMP_SCRIPT
   else
     bump_build
   fi
 }
-bump_build(){
+bump_build() {
   local version
-  local last
   local next
   local memo
-
-  if [ -f $BUMP_VERSION_FILE ]; then
-    last=$(cat $BUMP_VERSION_FILE)
-  else
-    last=0.0.0
-  fi
 
   bump_next
 
   case "$next" in
-    major)
-      version=$((${last%%.*} + 1)).0.0
-      ;;
-    minor)
-      memo=${last#*.}
-      version=${last%%.*}.$((${memo%%.*} + 1)).0
-      ;;
-    patch)
-      memo=${last##*.}
-      version=${last%.*}.$((${memo%%-*} + 1))
-      ;;
+  major)
+    version=$((${BUMP_LAST_VERSION%%.*} + 1)).0.0
+    ;;
+  minor)
+    memo=${BUMP_LAST_VERSION#*.}
+    version=${BUMP_LAST_VERSION%%.*}.$((${memo%%.*} + 1)).0
+    ;;
+  patch)
+    memo=${BUMP_LAST_VERSION##*.}
+    version=${BUMP_LAST_VERSION%.*}.$((${memo%%-*} + 1))
+    ;;
   esac
 
-  echo $version > $BUMP_VERSION_FILE
+  echo $version >$BUMP_VERSION_FILE
   git add $BUMP_VERSION_FILE
 }
-bump_next(){
+bump_next() {
   local only_ignored
-  local range
 
-  bump_commits_range
-
-  if [ -n "$(git log "$range" --format="%s" | grep "!" | head -1)" ]; then
+  if [ -f $BUMP_MAJOR_FILE ]; then
+    git rm $BUMP_MAJOR_FILE
     next=major
+    return
+  fi
+
+  bump_check_changes
+  if [ -n "$only_ignored" ]; then
+    next=patch
   else
-    bump_check_changes
-    if [ -n "$only_ignored" ]; then
-      next=patch
-    else
-      next=minor
-    fi
+    next=minor
   fi
 }
-bump_check_changes(){
+bump_check_changes() {
   local tmpdir
   local file
+  local range
 
   if [ -f $BUMP_IGNORE_FILE ]; then
-    tmpdir=.bump-version
+    tmpdir=.bump-version-check-changes-tmp-dir
 
     if [ -d $tmpdir ]; then
       return
@@ -78,6 +90,8 @@ bump_check_changes(){
 
     mkdir $tmpdir
     cp $BUMP_IGNORE_FILE $tmpdir/.gitignore
+
+    bump_commits_range
 
     for file in $(git diff "$range" --name-only); do
       mkdir -p $tmpdir/$(dirname $file)
@@ -95,53 +109,15 @@ bump_check_changes(){
   fi
 }
 
-bump_changelog(){
-  local changelog=CHANGELOG.md
-  local tmpfile=$changelog.tmp
-  local header="# CHANGELOG"
-
-  if [ -f $changelog ]; then
-    mv $changelog $tmpfile
-  fi
-
-  bump_changelog_content > $changelog
-
-  if [ -f $tmpfile ]; then
-    rm $tmpfile
-  fi
-
-  git add $changelog
-}
-bump_changelog_content(){
-  echo $header
-  echo ""
-  echo "## Version : $(cat $BUMP_VERSION_FILE)"
-  echo ""
-
-  bump_commits
-
-  if [ -f $tmpfile ]; then
-    cat $tmpfile | grep -v '^'"$header"'$'
-  fi
-}
-bump_commits(){
-  local range
-  local boundary=------------------------------BOUNDARY--
-
-  bump_commits_range
-
-  git log "$range" --merges --format="%b$boundary" | xargs /bin/echo | sed "s/$boundary \?/\n/g" | sed "s/See/: See/" | sed "s/^\(.\)/- \1/"
-}
-
-bump_commits_range(){
+bump_commits_range() {
   git fetch --tags --unshallow
-  if [ -n "$(git tag | head -1)" ]; then
-    range=$(git describe --abbrev=0 --tags)..
+  if [ -n "$(git tag -l $BUMP_LAST_VERSION)" ]; then
+    range=${BUMP_LAST_VERSION}..
   fi
 }
 
-
-bump_sync(){
+# helper command use in BUMP_SCRIPT
+bump_sync() {
   local target=$1
   local pattern=$2
 
@@ -149,8 +125,4 @@ bump_sync(){
   git add "$target"
 }
 
-
-bump_version
-bump_changelog
-
-git commit -m "version bumped: $(cat $BUMP_VERSION_FILE)"
+bump_main "$@"
